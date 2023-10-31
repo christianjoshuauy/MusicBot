abe6f13ee7b490d713db6c95dedfbfc341a5bf72
Dell <londonuts213@gmail.com>
2023-10-31 19:05:59 +0800

    Implemented search feature

---
+++ src/commands/search.js
@@ -0,0 +1,43 @@
+const { QueryType } = require("discord-player");
+const { tracksToDescription } = require("../functions");
+
+const search = async (interaction, player, queue) => {
+  await interaction.deferReply();
+
+  const query = interaction.options.get("query").value;
+  const searchResult = await player
+    .search(query, {
+      requestedBy: interaction.user,
+      searchEngine: QueryType.AUTO,
+    })
+    .catch(() => {});
+  if (!searchResult || !searchResult.hasTracks())
+    return void interaction.followUp({ content: "No results were found!" });
+
+  try {
+    if (!queue.connection)
+      await queue.connect(interaction.member.voice.channel);
+  } catch {
+    return void interaction.followUp({
+      content: "❌ | Could not join your voice channel!",
+    });
+  }
+
+  await interaction.followUp({
+    content: `⏱ | Loading your ${
+      searchResult.playlist ? "playlist" : "track"
+    } search results...`,
+  });
+
+  const embed = new EmbedBuilder()
+    .setTitle(`Your Search Results from - "${query}"!`)
+    .setThumbnail(tracks[0].thumbnail)
+    .setDescription(tracksToDescription(searchResult.tracks, true))
+    .setColor(Colors.Blue);
+
+  await queue.metadata.send({ embeds: [embed] });
+};
+
+module.exports = {
+  search,
+};
